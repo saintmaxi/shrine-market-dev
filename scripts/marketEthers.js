c4bcfc94b36d0fbf25e5c4790a98c162a25ae77f
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2022-07-17 07:41:22 -0500

    load listings when wallet not connected

--- scripts/marketEthers.js
+++ scripts/marketEthers.js
@@ -584,26 +584,28 @@ provider.on("network", async(newNetwork, oldNetwork) => {
     }
 });
 
+const loadAlchemyListings = async() => {
+    const jsonData = await fetch(`https://vxi8bzby29.execute-api.us-east-1.amazonaws.com/haki-market`).then(res => res.json());
+    $("#live-collections").empty();
+    $("#past-collections").empty();
+    $("#live-collections").append(jsonData.liveJSX);
+    $("#past-collections").append(jsonData.pastJSX);
+    $("#num-live").html(`<br>(${jsonData.numLive})`);
+    $("#num-past").html(`<br>(${jsonData.numPast})`);
+
+    if (jsonData.numLive == 0) {
+        $("#live-collections").append("<div id='no-live-msg'><h2>No active listings.<br>Join our Discord to see what's next!</h2><br><a href='https://discord.gg/theshrine' target='_blank'><button class='button'>JOIN DISCORD</button></a></div>");
+    }
+
+    idToListingInfo = new Map(jsonData.idToListingInfo);
+}
+
 window.onload = async() => {
     $("#claim-button").height($(".shrine-container:nth-child(2) img").height());
     $("#claim-button").removeClass("hidden");
     if (!(await getAddress())) {
-        const connectPrompt = ` <div id="ex1" class="partner-collection example">
-                                <div class="image-wrapper">
-                                    <svg id="info" onclick="openListingInfo()" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="white" d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c17.67 0 32 14.33 32 32c0 17.67-14.33 32-32 32S224 177.7 224 160C224 142.3 238.3 128 256 128zM296 384h-80C202.8 384 192 373.3 192 360s10.75-24 24-24h16v-64H224c-13.25 0-24-10.75-24-24S210.8 224 224 224h32c13.25 0 24 10.75 24 24v88h16c13.25 0 24 10.75 24 24S309.3 384 296 384z"/></svg>
-                                    <img class="collection-img" src="./images/silhouette.png">
-                                    <h4 id="minted-icon"><span id="ex-remaining">???</span>/<span id="ex-amount">???</span></h4>
-                                </div>
-                                <div class="collection-info">
-                                    <h3>???</h3>
-                                    <h4>??? <img src="https://github.com/saintmaxi/shrine-market-dev/blob/main/images/token.png?raw=true" class="token-icon"></h4>
-                                    <button class="button" onclick="connect()">CONNECT</button>
-                                </div>
-                            </div>`
-        $("#live-collections").empty();
-        $("#past-collections").empty();
-        $("#live-collections").append(connectPrompt);
-        $("#past-collections").append(connectPrompt);
+        console.log("using alchemy");
+        await loadAlchemyListings();
     }
     else {
         console.log("using wallet");
